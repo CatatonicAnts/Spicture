b673f91514a1296ac58de9d584c6b153f044b0b5
Saša Barišić <cartman300@net.hr>
2017-05-20 02:47:05 +0200

    Project files

---
+++ Spicture/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Spicture {
+	class Program {
+		static void Main(string[] args) {
+
+#if DEBUG
+			if (!Debugger.IsAttached)
+				Debugger.Launch();
+#endif
+
+
+		}
+	}
+}
